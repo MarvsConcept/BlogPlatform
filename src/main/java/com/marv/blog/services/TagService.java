bf246ac1871d93ca98b1796c2a3560187a474d02
MarvsConcept <Marvade8@gmail.com>
2025-11-10 05:23:19 -0800

    Delete Tag Endpoint

--- src/main/java/com/marv/blog/services/TagService.java
+++ src/main/java/com/marv/blog/services/TagService.java
@@ -4,8 +4,10 @@ import com.marv.blog.domain.entities.Tag;
 
 import java.util.List;
 import java.util.Set;
+import java.util.UUID;
 
 public interface TagService {
     List<Tag> getTags();
     List<Tag> createTags(Set<String> tagName);
+    void deleteTag(UUID id);
 }
